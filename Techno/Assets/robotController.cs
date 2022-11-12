6569f64a2ac2aeb39202860eaadef6a6b8d52b1d
Tilo <113525798+TiloWIjkniet@users.noreply.github.com>
2022-11-12 22:17:54 +0100

    puch

--- Techno/Assets/robotController.cs
+++ Techno/Assets/robotController.cs
@@ -2,184 +2,181 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class robotController : MonoBehaviour
-{   
+{
     [Tooltip("De tijt hoe lang een action duurt")]
     public float timeForAction = 1;
-    public float maxJumpHeight;
+    public float maxJumpHeight= 5;
     public float moveDistance = 1;
-
+    public float timeBetweenActions = 0.1f;
     public LayerMask layerMask;
-   
 
+
+    //Reverns variables
     private robotInstructions robotInstructions;
 
     //Global variables
     private float actionMultiplier;
-    public int countFremsDown;
+    [HideInInspector] public int countFremsDown;
     private int numberOfFremsToDessertDestination;
-    
+
     //Constant variables
     private int NUMBER_OF_FRAMS_IN_FIXED_UPDATE = 50;
-    
+
     //Rotation variables
     private float currentRoationY;
     private bool isTurning = false;
     private float turnAngel;
 
-
     //Move variables
     private bool isMoving = false;
     private Vector3 currentMovePosition;
     private float setMovePositionY;
-    
-    
+
     //Jump variables
+    private bool isJumpingForwart = false;
     private bool isJumping = false;
     private float framesCount = 0;
-    
-    // Racy variables 
+
+    // Racy variables
     private float RAYCAST_LENGTH_FRONT = 1f;
     private float RAYCAST_LENGTH_BELOW_FRONT = 0.6f;
     private float RAYCAST_LENGTH_BELOW = 10f;
 
-
-
-
-
-
     void Awake()
-    {
+    {   
         robotInstructions = GetComponent<robotInstructions>();
-        actionMultiplier = NUMBER_OF_FRAMS_IN_FIXED_UPDATE  / (NUMBER_OF_FRAMS_IN_FIXED_UPDATE * timeForAction);
-        numberOfFremsToDessertDestination = (int)(timeForAction * NUMBER_OF_FRAMS_IN_FIXED_UPDATE);
+        actionMultiplier = NUMBER_OF_FRAMS_IN_FIXED_UPDATE /(NUMBER_OF_FRAMS_IN_FIXED_UPDATE * timeForAction);
+        numberOfFremsToDessertDestination =(int)(timeForAction * NUMBER_OF_FRAMS_IN_FIXED_UPDATE);
         countFremsDown = numberOfFremsToDessertDestination;
     }
 
     void FixedUpdate()
     {
-        if(isTurning &&!isMoving && !isJumping)   HandelsRotation(turnAngel);
-        else if(isMoving && !isJumping )    HandelMovement(moveDistance);
-        else if(isJumping )   HandelJump(maxJumpHeight , moveDistance);
-        Debug.Log(isMoving);
+        if (isTurning && !isMoving && !isJumping&& !isJumpingForwart) HandelsRotation(turnAngel);
+        else if (isMoving && !isJumping && !isJumpingForwart)HandelMovement(moveDistance);
+        else if(!isJumping && isJumpingForwart) HandelJump(maxJumpHeight, moveDistance);
+        else if (isJumping) HandelJump(maxJumpHeight, 0);
     }
 
+    public void onFinichPrest()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position + new Vector3(0,0.5f,0), -transform.up ,out hit, 1);
+        Debug.Log(hit.collider.tag);
+        if(hit.collider.tag == "Finich")
+        {   
+            SceneManiger.ChangeScen(1);
+        }
+        else Debug.Log("hee"); 
+    }
 
-    public void OnTurnPrest(float _TurnAngel)
-    {   
+    public void ActionTurrn(float _TurnAngel)
+    {
         isTurning = true;
         turnAngel = _TurnAngel;
     }
-
-    public void OnMovePrest()
-    {    Debug.Log("test");
-        isMoving = true;
-        if(IsWalkable())
+    public void ActionMove()
+    {
+        if (IsWalkable())
         {
-            
-           
+            isMoving = true;
         }
-        
+        else robotInstructions.OnPlayPrest();
     }
-
-    public void OnJumpPrest()
-    {   
-        if(IsJumpable())
+    public void ActionJump()
+    {
+        isJumpingForwart = false;
+        if (IsJumpable())
+        {
+            isJumpingForwart = true;
+        }
+        else
         {
             isJumping = true;
         }
+        
     }
-
-
-
+    
     void HandelJump(float _MaxJumpHeight, float _DesiredPositionAfterAction)
     {
-       
-        framesCount ++;
-        float StartValu = - Mathf.Sqrt(_MaxJumpHeight);
-        float NextYHight = (-Mathf.Pow((StartValu + ((-StartValu*2) / numberOfFremsToDessertDestination)* framesCount),2)+ _MaxJumpHeight); // Formule: https://www.geogebra.org/classic/gg8vqdys
+        framesCount++;
+        float StartValu = -Mathf.Sqrt(_MaxJumpHeight);
+        float NextYHight =(-Mathf.Pow((StartValu +((-StartValu * 2) / numberOfFremsToDessertDestination) *framesCount),2) +_MaxJumpHeight); // Formule: https://www.geogebra.org/classic/gg8vqdys
         NextYHight += setMovePositionY;
-        if (currentMovePosition.y <= HeightObjectBelow () && framesCount >= numberOfFremsToDessertDestination /2)
+
+        if (framesCount >= numberOfFremsToDessertDestination)
         {
-            currentMovePosition.y = HeightObjectBelow ();
+            framesCount = 0;
+            currentMovePosition.y = HeightObjectBelow();
+            setMovePositionY = currentMovePosition.y;
+        }
+        else if (currentMovePosition.y <= HeightObjectBelow() &&framesCount >= numberOfFremsToDessertDestination / 2)
+        {
+            currentMovePosition.y = HeightObjectBelow();
         }
         else currentMovePosition.y = NextYHight;
-     
-        if(framesCount >= numberOfFremsToDessertDestination)
-        {
-            framesCount = 0; 
-            currentMovePosition.y = HeightObjectBelow ();
-            setMovePositionY = currentMovePosition.y;        
-        }
 
-        HandelMovement(_DesiredPositionAfterAction);
-        
+        HandelMovement (_DesiredPositionAfterAction);
     }
-
     void HandelMovement(float _DesiredPositionAfterAction)
-    {   
-       
-        float ChangePositionOnEachFrame = _DesiredPositionAfterAction / NUMBER_OF_FRAMS_IN_FIXED_UPDATE;
-        Vector3 nextPosition =  (transform.forward * ChangePositionOnEachFrame * actionMultiplier );
+    {
+        float ChangePositionOnEachFrame =_DesiredPositionAfterAction / NUMBER_OF_FRAMS_IN_FIXED_UPDATE;
+        Vector3 nextPosition =(transform.forward * ChangePositionOnEachFrame * actionMultiplier);
         currentMovePosition = currentMovePosition + nextPosition;
+
+        Debug.DrawLine(currentMovePosition + new Vector3 (0,0.5f,0) ,transform.position + new Vector3 (0,0.5f,0),Color.red , 100);
+
         transform.position = currentMovePosition;
         isMoving = IsActionActive();
-        
     }
-
     void HandelsRotation(float _DesiredRotationAfterAction)
     {
-        float ChangeAngelOnEachFrame = _DesiredRotationAfterAction / NUMBER_OF_FRAMS_IN_FIXED_UPDATE;
-        float NextRotationY = ChangeAngelOnEachFrame * actionMultiplier;
-        currentRoationY += NextRotationY;
-        transform.rotation = Quaternion.AngleAxis((currentRoationY),Vector3.up);
+        float ChangeAngelOnEachFrame =_DesiredRotationAfterAction / NUMBER_OF_FRAMS_IN_FIXED_UPDATE;
+        float NextRotationY = ChangeAngelOnEachFrame * actionMultiplier;currentRoationY += NextRotationY;
+        transform.rotation =Quaternion.AngleAxis((currentRoationY), Vector3.up);
         isTurning = IsActionActive();
     }
 
-
-
     bool IsWalkable()
-    {   
-
-        return !IsSomethingFront(RAYCAST_LENGTH_FRONT,transform.position+ new Vector3(0,0.5f,0)) && IsSomethingBelowFront(RAYCAST_LENGTH_BELOW_FRONT);
+    {
+        return !IsSomethingFront(RAYCAST_LENGTH_FRONT,transform.position + new Vector3(0, 0.25f, 0)) &&IsSomethingBelowFront(RAYCAST_LENGTH_BELOW_FRONT);
     }
-
     bool IsJumpable()
     {
-        return !IsWalkable() && IsSomethingBelowFront(maxJumpHeight /2 + RAYCAST_LENGTH_BELOW_FRONT) && !IsSomethingFront(RAYCAST_LENGTH_FRONT,transform.position+ new Vector3(0,maxJumpHeight /2 + RAYCAST_LENGTH_BELOW_FRONT,0));
+        return !IsWalkable() && IsSomethingBelowFront(maxJumpHeight / 2 + RAYCAST_LENGTH_BELOW_FRONT) &&!IsSomethingFront(RAYCAST_LENGTH_FRONT,transform.position +new Vector3(0, maxJumpHeight / 2 + RAYCAST_LENGTH_BELOW_FRONT, 0));
     }
-
-    bool IsSomethingFront(float _Length ,Vector3 _StartPosition)
+    bool IsSomethingFront(float _Length, Vector3 _StartPosition)
     {
-        
-        return  Physics.Raycast(_StartPosition, transform.forward, _Length ,layerMask) ;
+        return Physics.Raycast(_StartPosition, transform.forward, _Length, layerMask);
     }
-
     bool IsSomethingBelowFront(float _Length)
     {
-        Vector3 startPosition = transform.position +( transform.forward * 1.5f) + new Vector3(0,0.5f,0);
-        return  Physics.Raycast(startPosition, -transform.up, _Length ,layerMask) ;
+        Vector3 startPosition =transform.position +(transform.forward * 1f) +new Vector3(0, 1.5f, 0);
+        return Physics.Raycast(startPosition, -transform.up, _Length + 1.5f, layerMask);
     }
-
     bool IsActionActive()
-    {   
-        countFremsDown --;
-        if(countFremsDown <=0) 
-        {   
-            
+    {
+        countFremsDown--;
+        if (countFremsDown <= 0)
+        {
             countFremsDown = numberOfFremsToDessertDestination;
+            isJumpingForwart = false;
             isJumping = false;
             robotInstructions.OnPlayPrest();
-            return false;  
+            currentMovePosition = new Vector3 (Mathf.Round(currentMovePosition.x),Mathf.Round(currentMovePosition.y),Mathf.Round(currentMovePosition.z));
+            transform.position = currentMovePosition;
+            return false;
         }
         return true;
     }
-    
-    
-    float HeightObjectBelow ()
+
+    float HeightObjectBelow()
     {
         RaycastHit hit;
-        Physics.Raycast(transform.position + new Vector3(0,1,0), -transform.up , out hit, RAYCAST_LENGTH_BELOW , layerMask );
-        return transform.position.y - hit.distance +1;
+        Physics.Raycast(transform.position + new Vector3(0, 1, 0),-transform.up,out hit,RAYCAST_LENGTH_BELOW,layerMask);
+        return transform.position.y - hit.distance + 1;
     }
-    }
+
+}
--- Techno/Assets/robotInstructions.cs
+++ Techno/Assets/robotInstructions.cs
@@ -1,65 +1,121 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [System.Serializable]
 public class Instructions
 {
     public string function;
+
     public float turnAngel;
 
     public Instructions(string _Function, float _TurnAngel)
     {
-        function =_Function;
+        function = _Function;
         turnAngel = _TurnAngel;
     }
- 
+
     public Instructions(string _Function)
     {
-        function =_Function;
+        function = _Function;
     }
 
- 
+    
 }
 
 public class robotInstructions : MonoBehaviour
 {   
-    private robotController robotController;
-    
     public List<Instructions> instructions = new List<Instructions>();
-
+    public List<TextMeshProUGUI> instructionsText = new List<TextMeshProUGUI>();
+    private robotController robotController;
+    int curentInstructionsCount;
+    
     void Awake()
-    {
+    {   
         robotController = GetComponent<robotController>();
+        
     }
 
     public void OnTurnPrest(float _TurnAngel)
-    {   
-        instructions.Add(new Instructions("OnTurnPrest", _TurnAngel ));
-       
-    }
-
-    public void OnMovePrest()
-    {     
-        instructions.Add(new Instructions("OnMovePrest"));
-        
-    }
-
-    public void OnJumpPrest()
-    {   
-        instructions.Add(new Instructions("OnJumpPrest"));
-        
-    }
-
-    int i;
-    public void OnPlayPrest()
     {
         
-        if(instructions[i].function  == "OnTurnPrest" ){      robotController.OnTurnPrest(instructions[i].turnAngel); }
-        else if (instructions[i].function  == "OnMovePrest" ){robotController.OnMovePrest();}
-        else if (instructions[i].function  == "OnJumpPrest" ){robotController.OnJumpPrest();}
-        Debug.Log(i);
-        if(i <= instructions.Count)i++;
+        instructions.Add(new Instructions("Turn", _TurnAngel));
+        EditUI(instructions.Count-1);
+
+    }
+    public void OnMovePrest()
+    {
+        
+        instructions.Add(new Instructions("move"));
+        EditUI(instructions.Count-1);
+    }
+    public void OnJumpPrest()
+    {
+        
+        instructions.Add(new Instructions("jump"));
+        EditUI(instructions.Count-1);
+    }
+    public void onFinichPrest()
+    {
+        instructions.Add(new Instructions("Finich"));
+        EditUI(instructions.Count-1);
+    }
+    public void OnResetPrest()
+    {
+        SceneManiger.ChangeScen();
     }
 
+    void EditUI(int _I)
+    {   
+        instructionsText[_I].text = instructions[_I].function;
+        
+    }
+    void ResetInstructions()
+    {
+            for (int i = 0; i < instructions.Count; i++)
+            {
+                instructionsText[i].color = Color.black;
+                instructionsText[i].text = " ";
+            }  
+            curentInstructionsCount =0; instructions= new List<Instructions>();
+    }
+
+    public void OnPlayPrest()
+    {   
+
+
+        if(curentInstructionsCount >= instructions.Count) ResetInstructions();
+        else if(curentInstructionsCount < instructions.Count)  StartCoroutine(PlayActions()); 
+        
+    }
+
+    IEnumerator PlayActions()
+    {   
+        yield return new WaitForSeconds(robotController.timeBetweenActions);
+
+        if (instructions[curentInstructionsCount].function == "Turn")
+        {
+            robotController.ActionTurrn(instructions[curentInstructionsCount].turnAngel);
+        }
+        else if (instructions[curentInstructionsCount].function == "move")
+        {
+            robotController.ActionMove();
+        }
+        else if (instructions[curentInstructionsCount].function == "jump")
+        {
+            robotController.ActionJump();
+        }
+        else if (instructions[curentInstructionsCount].function == "Finich")
+        {   
+            robotController.onFinichPrest();
+        }
+
+
+        instructionsText[curentInstructionsCount].color = Color.red;
+
+        if (curentInstructionsCount < instructions.Count) curentInstructionsCount++;
+        
+        
+    }
 }
